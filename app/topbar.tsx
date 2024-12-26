45202dd83e0893a9bf5ff41f1fd60fb78544d9d1
faiz <me@faiz.im>
2024-12-26 22:27:36 +0900

    ➕존나 급함

--- app/topbar.tsx
+++ app/topbar.tsx
@@ -6,26 +6,28 @@ export default function Topbar() {
     return (
         <nav>
             <div className="font-PretendardVarible">
-                <div className="flex justify-center w-full items-center p-[0.625rem]">
-                    <div className="flex flex-grow justify-center">
-                        <Image src="icon.svg" alt="" width="50" height="49"/>
-                        <span className="text-black text-5xl font-extralight pr-3 pl-2">Refugee</span>
-                        <span className="text-[#009476] text-5xl font-extralight">Outreach Service</span>
+                <Link href={"/"}>
+                    <div className="flex justify-center w-full items-center p-[0.625rem]">
+                        <div className="flex flex-grow justify-center">
+                            <Image src="icon.svg" alt="" width="50" height="49"/>
+                            <span className="text-black text-5xl font-extralight pr-3 pl-2">Refugee</span>
+                            <span className="text-[#009476] text-5xl font-extralight">Outreach Service</span>
+                        </div>
                     </div>
-                </div>
-                <div className="flex justify-center">
-                    <div className="flex justify-between items-center w-2/3 pt-4">
-                        <Link href="/news">뉴스</Link>
-                        <Link href="/shelter">대피소 위치</Link>
-                        <Link href="/call">비상 연락망</Link>
-                        <Link href="/qna">Q&A</Link>
-                        <Link href="/lang">언어</Link>
-                        <Link href="/support">후원</Link>
+                    <div className="flex justify-center">
+                        <div className="flex justify-between items-center w-2/3 pt-4">
+                            <Link href="/news">뉴스</Link>
+                            <Link href="/shelter">대피소 위치</Link>
+                            <Link href="/call">비상 연락망</Link>
+                            <Link href="/qna">Q&A</Link>
+                            <Link href="/lang">언어</Link>
+                            <Link href="/support">후원</Link>
+                        </div>
                     </div>
-                </div>
-                <div>
+                    <div>
 
-                </div>
+                    </div>
+                </Link>
             </div>
         </nav>
     )
